4c33a231a076fe62cb9e8f7b99ecdaf89a254f8a
azirulislam <azirulislam27@gmail.com>
2022-05-22 00:36:22 +0600

    basic installation complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
     <div className="App">
       <h2>This is toolsmart </h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+      </Routes>
       
     </div>
   );
---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h3>Here some of our blogs</h3>
+        </div>
+    );
+};
+
+export default Blogs;
